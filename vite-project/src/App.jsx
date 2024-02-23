6d79c59fe4f4bc21b3cfd3be5dae213241c577bf
tushar801 <jaintushar801@gmail.com>
2024-02-23 07:47:17 -0800

    Pass LocationOfExpenditure as props and using a Map function

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css'
+import  ExpenseItem  from './Components/ExpenseItem'
+
+function App() {
+
+  const expenses = [
+    {id: 'e1', 
+     title: 'Food',
+     amount: 234,
+     date: new Date(2024, 2, 23),
+     LocationOfExpenditure: 'Restaurant',
+    },
+
+    {id: 'e2', 
+    title: 'Shopping',
+    amount: 1000,
+    date: new Date(2024, 2, 12),
+    LocationOfExpenditure: 'Mall',
+   },
+
+   {id: 'e3', 
+   title: 'Car',
+   amount: 4000,
+   date: new Date(2024, 2, 13),
+   LocationOfExpenditure: 'Showroom',
+  },
+
+  {id: 'e4', 
+  title: 'Travel',
+  amount: 504,
+  date: new Date(2024, 2, 20),
+  LocationOfExpenditure: 'Goa'
+ }
+  ];
+
+  return (
+    <>
+      <h2>Let's get started!</h2>
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          key={index}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          LocationOfExpenditure={expense.LocationOfExpenditure}
+        />
+      ))}
+      
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/ExpenseItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './ExpenseItem.css'
+
+function ExpenseItem(props){  
+
+
+  return (
+
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <h2>{props.LocationOfExpenditure}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  )
+}
+
+export default ExpenseItem;

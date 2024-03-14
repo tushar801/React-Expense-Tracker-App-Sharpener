6e57672effff20d1e8c7195989b30b46f2f2c4dc
tushar801 <jaintushar801@gmail.com>
2024-03-13 23:39:36 -0800

    Power the submit button of our form

--- vite-project/src/Components/NewExpense/ExpenseForm.jsx
+++ vite-project/src/Components/NewExpense/ExpenseForm.jsx
@@ -7,22 +7,40 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-    
+
+        
 
     const titleChangeHandler = (event) => {
        setEnteredTitle(event.target.value);
+       
     };
 
     const amountChangeHandler = (event) => {
         setEnteredAmount(event.target.value); 
+
+        
     };
 
     const dateChangeHandler = (event) => {
         setEnteredDate(event.target.value);
+
     };
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData);
+
+    }
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
               <div className='new-expense__control'>
                   <label>Title</label>
